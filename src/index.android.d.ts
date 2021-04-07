ddeef7579810eed347034ec79675fa01b88ced2d
Brad Martin <bradwaynemartin@gmail.com>
2021-04-07 16:17:14 -0500

    bump

--- src/index.android.d.ts
+++ src/index.android.d.ts
@@ -2,7 +2,7 @@ export declare class AndroidSensors {
     private XSensorClass;
     constructor(liteData?: boolean);
     setListener(listener: any): void;
-    startSensor(sensor: number, delay: number, maxReportLatency?: number): android.hardware.Sensor;
+    startSensor(sensor: number, delay: number, maxReportLatency?: number, isWakeUpSensor?: boolean): android.hardware.Sensor;
     stopSensor(sensor: android.hardware.Sensor): void;
     getDeviceSensors(): android.hardware.Sensor[];
     flush(): boolean;
